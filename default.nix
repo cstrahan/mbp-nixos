608e053e5337cde79ecab40dfa59ca02b8c76d24
Charles Strahan <charles.c.strahan@gmail.com>
2015-01-03 18:22:50 -0500

    various tweaks

--- default.nix
+++ default.nix
@@ -68,12 +68,15 @@ in rec {
 
         environment.systemPackages = [
           pkgs.wpa_supplicant_gui
+          pkgs.vim
         ];
 
         services.xserver.synaptics.enable = true;
         services.xserver.synaptics.twoFingerScroll = true;
         services.xserver.synaptics.buttonsMap = [ 1 3 2 ];
         services.xserver.synaptics.tapButtons = false;
+
+        networking.enableIPv6 = false;
 
         # Wireless drivers
         nixpkgs.config.allowUnfree = true;
@@ -96,6 +99,63 @@ in rec {
           "http://cache.nixos.org/"
           "http://hydra.nixos.org/"
         ];
+
+        nixpkgs.config.packageOverrides = pkgs:
+          rec {
+            vim =
+              with pkgs;
+              with pkgs.xorg;
+              let ruby = ruby_2_1_2; in
+              stdenv.mkDerivation rec {
+                name = "vim-${version}";
+
+                version = "7.4.481";
+
+                dontStrip = 1;
+
+                src = fetchhg {
+                  url = "https://vim.googlecode.com/hg/";
+                  rev = "v7-4-481";
+                  sha256 = "1fhcgdx8d6qxgi7a3xnicxvvs9k19raa6y4dyvlzam2clz43nz1s";
+                };
+
+                buildInputs = [
+                  pkgconfig gettext glib
+                  libX11 libXext libSM libXpm libXt libXaw libXau libXmu libICE
+                  gtk ncurses
+                  cscope
+                  python ruby luajit perl tcl
+                ];
+
+                configureFlags = [
+                    "--enable-cscope"
+                    "--enable-fail-if-missing"
+                    "--with-features=huge"
+                    "--enable-gui=none"
+                    "--enable-multibyte"
+                    "--enable-nls"
+                    "--enable-luainterp=yes"
+                    "--enable-pythoninterp=yes"
+                    "--enable-perlinterp=yes"
+                    "--enable-rubyinterp=yes"
+                    "--enable-tclinterp=yes"
+                    "--with-luajit"
+                    "--with-lua-prefix=${luajit}"
+                    "--with-python-config-dir=${python}/lib"
+                    "--with-ruby-command=${ruby}/bin/ruby"
+                    "--with-tclsh=${tcl}/bin/tclsh"
+                    "--with-tlib=ncurses"
+                    "--with-compiledby=Nix"
+                ];
+
+                meta = with stdenv.lib; {
+                  description = "The most popular clone of the VI editor";
+                  homepage    = http://www.vim.org;
+                  maintainers = with maintainers; [ cstrahan ];
+                  platforms   = platforms.unix;
+                };
+              };
+          };
       };
     type = "mbp";
     inherit system;
